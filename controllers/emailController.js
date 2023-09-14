a269399e624bdae4465693bf068b78e3dbc791f2
Solymady <117080143+Solymady@users.noreply.github.com>
2023-09-14 20:53:53 +0300

    mail

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -74,7 +74,7 @@ const verifyEmail = async (req, res) => {
                catch (e) {
                    console.log('maindb:'+ e)
                }
-               res.redirect("/admin/login");
+               res.redirect("http://localhost:3000/admin/login");
 
                // await user.save();
                // res.redirect("/login"); // Redirect to the login page after successful verification
